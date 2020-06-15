b751a5c2da93e5396dc7c67cda7f243da0329145
jadergermano <jader.germano@icloud.com>
2020-06-15 20:31:30 -0300

    Simple application for introductory concepts of reactJS

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import Routes from "./routes";
 
-function App() {
-  return (
+import "./styles.css"
+
+import Header from "./components/Header";
+
+const App = () => (
     <div className="App">
-        <h1>Hello World</h1>
+        <Header/>
+        <Routes/>
     </div>
-  );
-}
+);
 
 export default App;
